6e6a028bae18c1a9ca03f86b09b094121b9a459e
ArtemRe <RechembeiArtem@gmail.com>
2022-07-25 22:42:13 +0300

    ddddd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,10 @@
-// import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
+import storage from './storage';
 
 const form = document.querySelector('.feedback-form');
-
+unitForm();
 form.addEventListener('submit', onSubmit);
+
 function onSubmit(e) {
   e.preventDefault();
   const { email, message } = e.target.elements;
@@ -11,12 +13,24 @@ function onSubmit(e) {
   formData.forEach((value, email) => {
     userData[email] = value;
   });
-  console.log(userData);
   e.target.reset();
+  storage.remove('input_data');
 }
-form.addEventListener('input', onInput);
-userData = {};
+
+form.addEventListener('input', throttle(onInput, 300));
+
 function onInput(e) {
-  userData[e.target.email] = e.target.value;
-  localStorage.setItem('nimber', JSON.stringify(userData));
+  let saveData = storage.load('input_data');
+  saveData = saveData ? saveData : {};
+  saveData[e.target.name] = e.target.value;
+  storage.save('input_data', saveData);
+}
+
+function unitForm() {
+  const saveData = storage.load('input_data');
+  if (saveData) {
+    Object.entries(saveData).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }
 }
