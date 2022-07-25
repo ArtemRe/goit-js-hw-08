a1c5659f251496880219198f41f96f2424a913a1
ArtemRe <RechembeiArtem@gmail.com>
2022-07-25 07:24:36 +0300

    newww

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+// import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+  e.preventDefault();
+  const { email, message } = e.target.elements;
+  const formData = new FormData(form);
+  const userData = {};
+  formData.forEach((value, email) => {
+    userData[email] = value;
+  });
+  console.log(userData);
+  e.target.reset();
+}
+form.addEventListener('input', onInput);
+userData = {};
+function onInput(e) {
+  userData[e.target.email] = e.target.value;
+  localStorage.setItem('nimber', JSON.stringify(userData));
+}
